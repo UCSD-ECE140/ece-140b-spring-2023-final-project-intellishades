71e0d4f091bbf5ec5540949b5a5763185cf569ef
Ruilin Hu <ruhu@ucsd.edu>
2023-05-22 18:35:29 -0700

    Added Dimming on the cells

--- code/public/schedule.js
+++ code/public/schedule.js
@@ -32,14 +32,21 @@ var tableData = [
 var transparentButtonClicked = false;
 var blackButtonClicked = false;
 var dimButtonClicked = false;
+var buttonColor; // current color selected for the cell
 var currentCellStatus = [];
+for (var i = 0; i < 24; i++) {
+    currentCellStatus[i] = [];
+    for (var j = 0; j < 7; j++) {
+        currentCellStatus[i][j] = 2; // Set schedule to transparent on default
+    }
+}
 
 function init () { //only for things that are slow 
     // Button to choose window mode
     const transparentButton = document.getElementById("blueButton");
     const dimButton = document.getElementById("dimButton");
     const blackButton = document.getElementById("blackButton");
-    //var buttonColor;
+    var buttonColor;
     //var BlueOriginalColor = transparentButton.style.backgroundColor;
     //var BlackOriginalColor = blackButton.style.backgroundColor;
     //var DimOriginalColor = dimButton.style.backgroundColor;
@@ -48,43 +55,66 @@ function init () { //only for things that are slow
         if (blackButtonClicked) {
             //blackButton.style.backgroundColor = BlackOriginalColor;
             //blackButton.style.color = "#2C3531";
+            buttonColor = "none";
             blackButtonClicked = false;
             transparentButtonClicked = false;
             dimButtonClicked = false;
+            blackButton.classList.remove("selected-button");
+            transparentButton.classList.remove("selected-button");
+            dimButton.classList.remove("selected-button");
         } else {
             //blackButton.style.backgroundColor = "#2C3531";
             //blackButton.style.color = "white";
-            //buttonColor = "black"
+            buttonColor = "black";
             blackButtonClicked = true;
             transparentButtonClicked = false;
             dimButtonClicked = false;
+            blackButton.classList.add("selected-button");
+            transparentButton.classList.remove("selected-button");
+            dimButton.classList.remove("selected-button");
         }
     });
 
     transparentButton.addEventListener("click", function () {
         if (transparentButtonClicked) {
             //transparentButton.style.backgroundColor = BlueOriginalColor;
+            buttonColor = "none";
             blackButtonClicked = false;
             transparentButtonClicked = false;
             dimButtonClicked = false;
+            blackButton.classList.remove("selected-button");
+            transparentButton.classList.remove("selected-button");
+            dimButton.classList.remove("selected-button");
         } else {
             //transparentButton.style.backgroundColor = "#D1E8E2";
             //buttonColor = "blue"
+            buttonColor = "blue";
             blackButtonClicked = false;
             transparentButtonClicked = true;
             dimButtonClicked = false;
+            blackButton.classList.remove("selected-button");
+            transparentButton.classList.add("selected-button");
+            dimButton.classList.remove("selected-button");
         }
     });
 
     dimButton.addEventListener("click", function () {
         if (dimButtonClicked) {
+            buttonColor = "none";
             blackButtonClicked = false;
             transparentButtonClicked = false;
             dimButtonClicked = false;
+            blackButton.classList.remove("selected-button");
+            transparentButton.classList.remove("selected-button");
+            dimButton.classList.remove("selected-button");
         } else {
+            buttonColor = "dim";
             blackButtonClicked = false;
             transparentButtonClicked = false;
             dimButtonClicked = true;
+            blackButton.classList.remove("selected-button");
+            transparentButton.classList.remove("selected-button");
+            dimButton.classList.add("selected-button");
         }
     });
 
@@ -102,46 +132,46 @@ function populateTable() {
     tableDiv.appendChild(table);
 
     // Create table rows and cells and add them to the table
-    for (var i = 0; i < tableData.length; i++) {
+    // Must use let instead of var!!!
+    for (let i = 0; i < tableData.length; i++) {
         var row = document.createElement("tr");
-        for (var j = 0; j < tableData[i].length; j++) {
+        for (let j = 0; j < tableData[i].length; j++) {
             var cell = document.createElement("td");
             cell.innerText = tableData[i][j];
             if (i > 0 && j > 0) {
                 // Add event listener to cell
+                /*
                 var isMouseDown = false;
                 cell.addEventListener("mousedown", function () {
-                    isMouseDown = true;
+                    //isMouseDown = true;
                     this.classList.toggle(buttonColor);
                 });
                 cell.addEventListener("mouseover", function () {
-                    if (isMouseDown) {
+                    //if (isMouseDown) {
                         this.classList.toggle(buttonColor);
-                    }
-                });
+                    //}
+                });*/
                 cell.addEventListener("mouseup", function () {
-                    isMouseDown = false;
-                    cellFunctionality(i - 1, j - 1); // Add functionality to the cell
+                    this.classList.remove("black");
+                    this.classList.remove("blue");
+                    this.classList.remove("dim");
+                    //isMouseDown = false;
+                    let hour = i - 1;
+                    let day = j - 1;
+                    if (blackButtonClicked) { // If the user selected black to adjust
+                        currentCellStatus[hour][day] = 0;
+                        this.classList.add("black");
+                    } else if (transparentButtonClicked) { // If the user selected transparent to adjust
+                        currentCellStatus[hour][day] = 2;
+                        this.classList.add("blue");
+                    } else if (dimButtonClicked) { // If the user selected dim to adjust
+                        currentCellStatus[hour][day] = 1;
+                        this.classList.add("dim");
+                    }
                 });
             }
             row.appendChild(cell);
         }
         table.appendChild(row);
-    }
-}
-
-/**
- * This functionality updates the cell's status on the global variable 
- * @param int hour, the hour to modify by clicking the cell
- * @param int day, the day to modify by clicking the cell
- */
-function cellFunctionality(hour, day) {
-    // Do not modify cell data if no state is selected by the user
-    if (blackButtonClicked) { // If the user selected black to adjust
-        currentCellStatus[hour][day] = 0;
-    } else if (transparentButtonClicked) { // If the user selected transparent to adjust
-        currentCellStatus[hour][day] = 2;
-    } else if (dimButtonClicked) { // If the user selected dim to adjust
-        currentCellStatus[hour][day] = 1;
     }
 }
